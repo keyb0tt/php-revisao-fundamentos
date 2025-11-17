a906d6f9e13c776be704f52c05abe567b6005afa
kaicbosco <kaiqueboscoaraujo00@gmail.com>
2025-11-17 13:42:51 -0300

    .

--- README.md
+++
@@ -1,1 +0,0 @@
-# php-revisao-fundamentos
---
+++ exercicio1.php
@@ -0,0 +1,8 @@
+<?php
+// Crie variáveis para armazenar:
+// - Seu nome
+// - Sua idade
+// - Sua cidade
+// Exiba: "Meu nome é [nome], tenho [idade] anos e moro em [cidade]"
+
+?>
