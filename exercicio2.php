037d76b73f392c0d4955ab935a859fb3f35ee1ec
kaicbosco <kaiqueboscoaraujo00@gmail.com>
2025-11-17 14:02:06 -0300

    .

---
+++ exercicio2.php
@@ -0,0 +1,12 @@
+<?php
+  $n1 = 10;
+  $n2 = 5;
+
+  $soma = $n1 + $n2;
+  $sub = $n1 - $n2;
+  $mult = $n1 * $n2;
+  $div = $n1 / $n2;
+  $resto = $n1 % $n2;
+
+  print("Soma:$soma | Subtração:$sub | Multiplicação:$mult | Divisão:$div | Resto:$resto");
+?>
