7c7ea7c68c72a955d51002a318cd3d7083e50dae
kaicbosco <kaiqueboscoaraujo00@gmail.com>
2025-11-17 13:48:58 -0300

    .

--- exercicio1.php
+++ exercicio1.php
@@ -1,8 +1,10 @@
+<!-- Exercício 1: Variáveis e Saída Básica
+Crie variáveis para armazenar seu nome, idade e cidade.
+Exiba na tela a mensagem: "Meu nome é [nome], tenho [idade] anos e moro em [cidade]". -->
 <?php
-// Crie variáveis para armazenar:
-// - Seu nome
-// - Sua idade
-// - Sua cidade
-// Exiba: "Meu nome é [nome], tenho [idade] anos e moro em [cidade]"
+  $nome = "Kaique";
+  $idade = "20";
+  $cidade = "Birigui";
 
+  print("Meu nome é $nome, tenho $idade anos e moro em $cidade");
 ?>
