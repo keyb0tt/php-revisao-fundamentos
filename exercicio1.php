de23ef519c67058fe34fa44dad203b79bb8c7aa0
kaicbosco <kaiqueboscoaraujo00@gmail.com>
2025-11-17 13:55:47 -0300

    .

--- exercicio1.php
+++ exercicio1.php
@@ -1,6 +1,3 @@
-<!-- Exercício 1: Variáveis e Saída Básica
-Crie variáveis para armazenar seu nome, idade e cidade.
-Exiba na tela a mensagem: "Meu nome é [nome], tenho [idade] anos e moro em [cidade]". -->
 <?php
   $nome = "Kaique";
   $idade = "20";
